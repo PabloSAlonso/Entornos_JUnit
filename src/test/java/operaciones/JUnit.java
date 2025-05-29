ec97ae4cca986cc6f014631cf5aaf9b1ab5c1d95
PabloSAlonso <santanaalonsopablo@gmail.com>
2025-05-29 12:07:59 +0200

    changes

--- src/test/java/operaciones/JUnit.java
+++ src/test/java/operaciones/JUnit.java
@@ -1,18 +1,25 @@
 package operaciones;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Pruebas")
 public class JUnit {
-    String cadena = "hola";
+    static String cadena;// Iniciar en before all pq es static se hace asi
 
-    //Ejercicio 1
+    @BeforeAll
+    static void pedirAntes() {
+        cadena = "hola";
+    }
+
+    // Ejercicio 1
     @Test
     public void testBisiesto() {
         int[] añosBisiestos = { 2000, 2004 };
@@ -42,7 +49,8 @@ public class JUnit {
         assertEquals(Math.pow(2, -2), Pruebas.potencia(2, 0), 0);
 
     }
-    //Ejercicio 2
+
+    // Ejercicio 2
     @Test
     public void testSubCadena() {
         assertEquals("hola", Pruebas.subCadena(cadena, 0, 4));
@@ -79,10 +87,76 @@ public class JUnit {
         }
 
     }
-    //Ejercicio 3
-    @Test
-    public void testFunciones() {
-        
+
+    // Ejercicio 3
+    int[] vector1;
+    int[] vector2;
+    int[] vector3;
+
+    @BeforeEach
+    void AntesDeCadaUno() {
+        vector1 = new int[] { 1, 2, 3, 4, 0 };
+        vector2 = new int[] { 4, 4, 4, 4, 4 };
+        vector3 = new int[] { -1, -2, -3, -4, -5 };
     }
 
+    @Test
+    public void testNumMayor() {
+        assertEquals(4, Pruebas.numeroMayor(vector1));
+        assertEquals(4, Pruebas.numeroMayor(vector2));
+        assertEquals(-1, Pruebas.numeroMayor(vector3));
+    }
+
+    @Test
+    public void testNumMenor() {
+        assertEquals(0, Pruebas.numeroMenor(vector1));
+        assertEquals(4, Pruebas.numeroMenor(vector2));
+        assertEquals(-5, Pruebas.numeroMenor(vector3));
+    }
+
+    @Test
+    public void testNumFails() {
+
+        try {
+            assertEquals(3, Pruebas.numeroMenor(vector1));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Expected equivocado");
+        }
+
+        try {
+            assertEquals(3, Pruebas.numeroMenor(vector2));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Expected equivocado");
+        }
+
+        try {
+            assertEquals(-1, Pruebas.numeroMenor(vector3));
+        } catch (IllegalArgumentException e) {
+            System.err.println("Expected equivocado");
+        }
+
+    }
+
+    @Test
+    public void testIntercambiaPosiciones() {
+        assertTrue(Pruebas.intercambiarDatos(vector1, 2, 4));
+        assertTrue(Pruebas.intercambiarDatos(vector1, 0, 4));
+        assertTrue(Pruebas.intercambiarDatos(vector1, 4, 0));
+        assertTrue(Pruebas.intercambiarDatos(vector1, 2, 4));
+    }
+
+    //#region MOCKS
+    public int[] ordenaVector(int[] v) {
+        return v;
+    }
+
+    public int sumaRango(int[] v, int inicio, int fin) {
+        return 0;
+    }
+    // #endregion
+
+    @Test
+    public void testOrdenaVector() {
+        assertArrayEquals(new int[] {0, 1, 2, 3, 4}, ordenaVector(vector1), "Se ordena");
+    }
 }
